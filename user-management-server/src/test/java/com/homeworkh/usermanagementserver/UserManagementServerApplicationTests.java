ee1b8c232547efc5681a3c0449547a4b2d73264f
lq-z <993055900@qq.com>
2020-12-18 22:36:58 +0800

    第一次提交用户管理服务

---
+++ user-management-server/src/test/java/com/homeworkh/usermanagementserver/UserManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeworkh.usermanagementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
