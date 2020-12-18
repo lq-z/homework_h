5026e230d6fba97f75756fabceacce75c929f11a
lq-z <993055900@qq.com>
2020-12-18 22:38:03 +0800

    第一次提交业务服务

---
+++ profession-server/src/main/java/com/homeworkh/professionserver/ProfessionServerApplication.java
@@ -0,0 +1,13 @@
+package com.homeworkh.professionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfessionServerApplication.class, args);
+    }
+
+}
---
+++ profession-server/src/test/java/com/homeworkh/professionserver/ProfessionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeworkh.professionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
